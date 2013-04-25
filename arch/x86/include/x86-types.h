e271cf29580241a30b212db5e58a2d0cbe6641c0
Chris Cummins <chrisc.101@gmail.com>
2013-04-25 09:56:21 +0100

    include: Define integer types for x86 arch

---
+++ arch/x86/include/x86-types.h
@@ -0,0 +1,19 @@
+/*
+ * Typedefs for use in the kernel.
+ */
+
+#ifndef _x86_TYPES_H
+#define _x86_TYPES_H
+
+/* Integer sizes */
+typedef unsigned char   u8;
+typedef          char   s8;
+typedef unsigned short  u16;
+typedef          short  s16;
+typedef unsigned int    u32;
+typedef          int    s32;
+
+/* Port */
+typedef u16 port_t;
+
+#endif /* _x86_TYPES_H */
---
+++ include/types.h
@@ -0,0 +1,17 @@
+/*
+ * Kernel opaque data types.
+ */
+
+#ifndef _TYPES_H
+#define _TYPES_H
+
+/*
+ * x86 data types.
+ */
+#ifdef __ARCH_x86__
+#include <x86-types.h>
+#endif
+
+typedef unsigned int size_t;
+
+#endif /* _TYPES_H */
