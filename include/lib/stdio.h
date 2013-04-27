a4476f2d3c87e796fd1e8e4286dfd8e5e5249282
Chris Cummins <chrisc.101@gmail.com>
2013-04-27 17:49:18 +0100

    lib: Add stdio.h and implementation
    This adds a standard input/output header with a minimalist
    implementation. At the moment, the stdio implementation uses the tty
    subsystem directly to write out to video memory, there is no file or
    stdout/stderr abstraction.

---
+++ include/lib/stdio.h
@@ -0,0 +1,77 @@
+#ifndef _STDIO_H
+#define _STDIO_H
+
+#include <stddef.h>
+#include <stdarg.h>
+
+/* The printf() and vprintf() functions produce output according to a format as
+ * described below by writing output to kstream.
+ *
+ * RETURN VALUE
+ *
+ * 	Upon successful return, these functions return the number of characters
+ * 	printed (excluding the null byte used to end output to strings).
+ *
+ * 	The functions snprintf() and vsnprintf() do not write more than size
+ * 	bytes (including the terminating null byte ('\0')). If the output was
+ * 	truncated due to this limit then the return value is the number of
+ * 	characters (excluding the terminating null byte) which would have been
+ * 	written to the final string if enough space had been available. Thus, a
+ * 	return value of size or more means that the output was truncated.
+ *
+ * 	If an output error is encountered, a negative value is returned.
+ *
+ * FORMAT OF THE FORMAT STRING
+ *
+ * 	The format string is a character string, beginning and ending in its
+ * 	initial shift state, if any. The format string is composed of zero or
+ * 	more directives: ordinary characters (not %), which are copied unchanged
+ * 	to the output stream; and conversion specifications, each of which
+ * 	results in fetching zero or more subsequent arguments. Each conversion
+ * 	specification consists of the character %, followed by a conversion
+ * 	character.
+ *
+ * 	The arguments must correspond properly (after type promotion) with the
+ * 	conversion specifier. By default, the arguments are used in the order
+ * 	given, where each '*' and each conversion specifier asks for the next
+ * 	argument (and it is an error if insufficiently many arguments are
+ * 	given).
+ *
+ * THE CONVERSION CHARACTER
+ *
+ * 	A character that specifies the type of conversion to be applied. The
+ * 	conversion specifiers and their meanings are:
+ *
+ * 	d, i  	The int argument is converted to signed decimal notation.
+ *
+ * 	x, X	The unsigned int argument is converted to unsigned hexadecimal
+ * 		(x and X) notation.
+ *
+ * 	c	The int argument is converted to an unsigned char, and the
+ * 		resulting character is written.
+ *
+ * 	s	The const char * argument is expected to be a pointer to an
+ * 		array of character type (pointer to a string). Characters from
+ * 		the array are written up to (but not including) a terminating
+ * 		null byte ('\0').
+ *
+ *	n	The number of characters written so far is stored into the
+ * 		integer indicated by the int * (or variant) pointer argument. No
+ * 		argument is converted.
+ *
+ * 	%	A '%' is written. No argument is converted. The complete
+ * 		conversion specification is '%%'.
+ */
+int printf(const char *format, ...);
+int vprintf(const char *format, va_list ap);
+int nprintf(size_t size, const char *format, ...);
+int nvprintf(size_t size, const char *format, va_list ap);
+
+/* Print a single character */
+void putchar(const char c);
+
+/* Provide convenient debugging calls. These are not disabled when NDEBUG is
+ * defined at build time */
+int debug(const char *format, ...);
+
+#endif /* _STDIO_H */
---
+++ lib/stdio.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+
+#include <kernel/tty.h>
+
+#include <string.h>
+#include <types.h>
+
+#define INT32_MAX_HEX_DIGITS 8
+#define INT32_MAX_DEC_DIGITS 10
+
+/* buffer used to store characters during format conversions */
+static char buf[INT32_MAX_DEC_DIGITS + 2];
+
+/* print a string */
+static inline void puts(const char *s)
+{
+	int i = 0;
+	u16 overflow = 0;
+
+	while (s[i] != '\0') {
+		putchar(s[i++]);
+
+		/* overflow counter to prevent infinite loop for non-null
+		 * terminated string */
+		return_if_fail(overflow = -1);
+		overflow++;
+	}
+}
+
+/* print an unsigned integer (decimal) */
+static void putu(const unsigned int u)
+{
+	unsigned int u_local = u;
+	char *c = buf + INT32_MAX_DEC_DIGITS + 1;
+
+	*c = '\0'; /* start with the string terminator and work backwards*/
+
+	do {
+		*--c = '0' + (u_local % 10);
+		u_local /= 10;
+	} while (u_local > 0);
+
+	puts(c);
+}
+
+/* print a signed integer */
+static void putd(const int d)
+{
+	if (d >= 0) {
+		putu((unsigned int)d);
+	} else {
+		putchar('-'); /* negative sign */
+		putu((unsigned int)-d);
+	}
+}
+
+/* print an unsigned integer (hexadecimal)*/
+static void putx(const unsigned int x)
+{
+	unsigned int x_local = x;
+	static char buf[INT32_MAX_HEX_DIGITS + 2];
+	char *c = buf + INT32_MAX_HEX_DIGITS + 1;
+
+	*c = '\0'; /* start with the string terminator and work backwards*/
+
+	do {
+		u8 nibble = x_local & 0x0F;
+
+		if (nibble < 0xA)
+			*--c = 0x30 + nibble;
+		else
+			*--c = 0x37 + nibble;
+
+		x_local = x_local >> 4;
+	} while (x_local != 0);
+
+	puts(c);
+}
+
+/* print a pointer */
+static void putp(const void *p)
+{
+	putx((unsigned int)p);
+}
+
+void putchar(const char c)
+{
+	tty_putc(c);
+}
+
+int nvprintf(size_t size, const char *format, va_list ap)
+{
+	u32 i;
+
+	return_val_if_fail(format != NULL, -1);
+
+	for (i = 0; i < size; i++) {
+		if (format[i] == '%') {
+			if ((i + 1) < size && format[i+1] != '%') {
+				char conv = format[i+1];
+
+				switch (conv) {
+
+				case 'c':
+				{
+					char c = va_arg(ap, char);
+					putchar(c);
+					i++;
+					break;
+				}
+
+				case 'u':
+				{
+					unsigned int u = va_arg(ap,
+								unsigned int);
+					putu(u);
+					i++;
+					break;
+				}
+
+				case 'x':
+				case 'X':
+				{
+					unsigned int x = va_arg(ap,
+								unsigned int);
+					putx(x);
+					i++;
+					break;
+				}
+
+				case 'd':
+				case 'i':
+				{
+					int d = va_arg(ap, int);
+					putd(d);
+					i++;
+					break;
+				}
+
+				case 'p':
+				{
+					void *p = va_arg(ap, void *);
+					putp(p);
+					i++;
+					break;
+				}
+
+				case 's':
+				{
+					const char *s = va_arg(ap, char *);
+					puts(s);
+					i++;
+					break;
+				}
+				}
+			}
+		} else {
+			putchar(format[i]);
+		}
+	}
+	return 0;
+}
+
+int nprintf(size_t size, const char *format, ...)
+{
+	int ret;
+	va_list ap;
+
+	va_start(ap, format);
+	ret = nvprintf(size, format, ap);
+	va_end(ap);
+
+	return ret;
+}
+
+int vprintf(const char *format, va_list ap)
+{
+	return nvprintf(strlen(format), format, ap);
+}
+
+int printf(const char *format, ...)
+{
+	int ret;
+	va_list ap;
+
+	va_start(ap, format);
+	ret = vprintf(format, ap);
+	va_end(ap);
+
+	return ret;
+}
+
+#ifndef NDEBUG
+int debug(const char *format, ...)
+{
+	int ret;
+	va_list ap;
+
+	va_start(ap, format);
+	ret = vprintf(format, ap);
+	va_end(ap);
+
+	/* auto-append newline */
+	putchar('\n');
+
+	return ret;
+}
+#else
+__diagnostic_disable(unused-parameter)
+inline int debug(const char *format, ...)
+{
+return 0;
+}
+__diagnostic_enable(unused-parameter)
+#endif /* NDEBUG */
