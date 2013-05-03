3757970339c147b16985b3d075801a91f8a2b175
Chris Cummins <chrisc.101@gmail.com>
2013-05-03 21:47:15 +0100

    arch/x86: Add a backtrace mechaniscm
    This walks up the stack, printing the location of each stack frame.

---
+++ arch/x86/kernel/bt.c
@@ -0,0 +1,34 @@
+#include <kernel/panic.h>
+
+#include <macros.h>
+#include <stdio.h>
+#include <types.h>
+
+#define MAX_STACK_FRAME 10000
+
+/*
+ * Walk up the stack and print the locations of each stack frame base.
+ */
+void bt(void) {
+	u32 *ebp, *eip;
+	unsigned int sf = 0;
+
+	/* Start form the current stack base */
+	asm volatile("mov %%ebp, %0" : "=r" (ebp));
+
+	/* Continue until you reach a zeroed stack base pointer. The initial ebp
+	 * value is zeroed at boot, but note that this could be corrupted in the
+	 * case of serious memory corruption. To protect against this, we keep
+	 * track of the frame number and break if it exceeds a reasonable
+	 * maximum value. */
+	while (ebp) {
+		eip = ebp + 1;
+		printf("#%d  [0x%x]\n", sf, *eip);
+		ebp = (u32*)*ebp;
+		sf++;
+
+		/* break if exceeded maximum */
+		if (sf > MAX_STACK_FRAME)
+			break;
+	}
+}
