daf2ca475a13849b4f0a7ae9f3c15bf05bd06a6a
Chris Cummins <chrisc.101@gmail.com>
2013-05-03 22:24:58 +0100

    tools: Add a headercheck script
    This script generates a list of all header files, and then iterates over
    each header and source file, listing those which include the header. The
    idea here is that it provides a convenient means for checking to see how
    widely used a source file is. For example, this tool could be used as a
    reference before changing a function prototype, to see how many source
    files will be affected.

---
+++ tools/headercheck.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#	headercheck.sh - List all header files and the sources
+#                        that include them
+#
+
+h_files="$(find . -name '*.h')"
+c_files="$(find . -name '*.c')"
+
+for h in $h_files; do
+	header=$(basename "$h")
+	echo "HEADER $h"
+
+	# Check first for header file includes
+	for hin in $h_files; do
+		grep -E "\s*#\s*include\s+<.*$header\s*>" \
+			"$hin" &>/dev/null && {
+			echo -e "\tHEADER "$hin
+		}
+	done
+
+	# Then check for source file includes
+	for cin in $c_files; do
+		grep -E "\s*#\s*include\s+<.*$header\s*>" \
+			"$cin" &>/dev/null && {
+			echo -e "\tSOURCE "$cin
+		}
+	done
+	echo ""
+done
