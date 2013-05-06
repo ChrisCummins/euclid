5121e8463ede5072bee3a31bed6f7e9e793cd183
Chris Cummins <chrisc.101@gmail.com>
2013-05-06 22:51:56 +0100

    lib: Add ELF format data structures and procedures

---
+++ include/lib/elf.h
@@ -0,0 +1,49 @@
+/*
+ * ELF format data structures and procedures.
+ */
+
+#ifndef _ELF_H
+#define _ELF_H
+
+#include <macros.h>
+#include <types.h>
+
+#define ELF32_ST_TYPE(i) ((i)&0xf)
+
+__diagnostic_disable(packed)
+
+struct elf_section {
+	u32 name;
+	u32 type;
+	u32 flags;
+	u32 addr;
+	u32 offset;
+	u32 size;
+	u32 link;
+	u32 info;
+	u32 addralign;
+	u32 entsize;
+} __attribute__((packed));
+
+struct elf_symbol {
+	u32 name;
+	u32 value;
+	u32 size;
+	u8 info;
+	u8 other;
+	u16 shndx;
+} __attribute__((packed));
+
+__diagnostic_enable(packed)
+
+struct elf {
+	struct elf_symbol *symtab;
+	u32 symtabsz;
+	const char *strtab;
+	u32 strtabsz;
+};
+
+/* Look up a symbol by address */
+const char *symbol_from_elf(struct elf *elf, u32 addr);
+
+#endif /* _ELF_H */
---
+++ lib/elf.c
@@ -0,0 +1,23 @@
+#include <elf.h>
+
+#include <kernel/panic.h>
+
+#include <stddef.h>
+
+const char *symbol_from_elf(struct elf *elf, u32 address) {
+	unsigned int i;
+
+	for (i = 0; i < (elf->symtabsz / sizeof(struct elf_symbol)); i++) {
+		if (ELF32_ST_TYPE(elf->symtab[i].info) != 0x2)
+			continue;
+
+		if ((address >= elf->symtab[i].value) &&
+		    (address < (elf->symtab[i].value + elf->symtab[i].size))) {
+			return (const char *)((u32)elf->strtab
+					      + elf->symtab[i].name);
+		}
+	}
+
+	printf("Unable to find address %x in elf\n");
+	return NULL;
+}
