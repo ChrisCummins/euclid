e97ab1420b6719e89e6aa4ff943786e6adeeefa8
Chris Cummins <chrisc.101@gmail.com>
2013-05-07 23:12:56 +0100

    lib/macros: Remove unnecessary whitespace

--- include/lib/macros.h
+++ include/lib/macros.h
@@ -92,7 +92,7 @@
  */
 #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)
 
-#define __do_pragma(x)			_Pragma (#x)
+#define __do_pragma(x)			_Pragma(#x)
 #define __diagnostic_pragma(x)		__do_pragma(GCC diagnostic x)
 #define __message_pragma(x)		__do_pragma(message __stringify(x))
 
