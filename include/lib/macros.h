540381eed65fe1d665b9c763bc9b61e8378c052e
Chris Cummins <chrisc.101@gmail.com>
2013-05-08 02:28:23 +0100

    include/lib/macros.h: Add Doxygen documentation

--- include/lib/macros.h
+++ include/lib/macros.h
@@ -1,4 +1,6 @@
-/*
+/**
+ * @file macros.h
+ *
  * Common macros definitions
  *
  * This file must not include any other header files, as many will rely on it.
@@ -7,12 +9,24 @@
 #ifndef _MACROS_H
 #define _MACROS_H
 
-/* Provide a means to test if a character is a digit. */
+/**
+ * Provide a means to test if a character is a digit.
+ */
 #define is_digit(c) ((c) >= '0' && (c) <= '9')
 
-/* Helper macros */
+/**
+ * Stringify (works on strings and macros).
+ */
 #define __stringify(x)	___stringify(x)
+
+/**
+ * Internal stringification.
+ */
 #define ___stringify(x) #x
+
+/**
+ * Concatenate two strings.
+ */
 #define __concat(x, y)	__stringify(x ## y)
 
 /*
@@ -21,14 +35,33 @@
  *   See: http://gcc.gnu.org/onlinedocs/gcc-4.3.4/gcc/Alternate-Keywords.html
  */
 #ifdef __GNUC__
+/**
+ * Re-enables disabled keyword. See <a
+ href="http://gcc.gnu.org/onlinedocs/gcc-4.3.4/gcc/Alternate-Keywords.html"
+ target="_blank">Alternate Keywords (GCC Manual)</a> for more information.
+ */
 #define asm		__asm__
+
+/**
+ * Re-enables disabled keyword. See <a
+ href="http://gcc.gnu.org/onlinedocs/gcc-4.3.4/gcc/Alternate-Keywords.html"
+ target="_blank">Alternate Keywords (GCC Manual)</a> for more information.
+ */
 #define inline		__inline__
+
+/**
+ * Re-enables disabled keyword. See <a
+ href="http://gcc.gnu.org/onlinedocs/gcc-4.3.4/gcc/Alternate-Keywords.html"
+ target="_blank">Alternate Keywords (GCC Manual)</a> for more information.
+ */
 #define typeof		__typeof__
 #endif /* __GNUC__ */
 
-/* Branch prediction performance optimisations */
 #if defined(__GNUC__) && (__GNUC__ > 2) && defined(NDEBUG)
 
+/**
+ * Cast any true-false expression to boolean.
+ */
 #define __boolean_expression(x)			\
 	({ int __boolean;			\
 	   if (x)				\
@@ -36,29 +69,51 @@
 	   else					\
 		   __boolean = 0;		\
 	   __boolean; })
-#define likely(x)	__builtin_expect(__boolean_expression(x), 1)
-#define unlikely(x)	__builtin_expect(__boolean_expression(x), 0)
+
+/**
+ * Optimise for true branch.
+ */
+#define likely(x) __builtin_expect(__boolean_expression(x), 1)
+
+/**
+ * Optimise for false branch.
+ */
+#define unlikely(x) __builtin_expect(__boolean_expression(x), 0)
 
 #else /* not GNUC */
 
-#define likely(x)	(x)
-#define unlikely(x)	(x)
+/**
+ * Optimise for true branch.
+ */
+#define likely(x) (x)
+
+/**
+ * Optimise for false branch.
+ */
+#define unlikely(x) (x)
 
 #endif /* __GNUC__ */
 
-/* Test if an address is within a given region of memory */
-#define __is_in_region(addr, base, end)		\
+/**
+ * Test if an address is within a given region of memory.
+ */
+#define __is_in_region(addr, base, end)			\
 	(((addr) >= (base)) && ((addr) <= (end)))
 
-/* Provide means for early exiting of a function if an expression is false */
 #ifndef NDEBUG
 
+/**
+ * Provide means for early exiting of a function if an expression is false.
+ */
 #define return_if_fail(x)						\
 	if (unlikely(!(x))) {						\
 		printf(STRLOC " return_if_fail(" #x ")");		\
 		return;							\
 	}
 
+/**
+ * Provide means for early exiting of a function if an expression is false.
+ */
 #define return_val_if_fail(x, val)					\
 	if (unlikely(!(x))) {						\
 		printf(STRLOC " return_val_if_fail(" #x ", " #val")");	\
@@ -72,8 +127,10 @@
 #endif /* __OPTIMIZE__ */
 
 
-/* Protect against compiling without GCC */
 #ifndef __GNUC__
+/**
+ * Protect against compiling without GCC
+ */
 #define __attribute__
 #endif
 
@@ -92,17 +149,43 @@
  */
 #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)
 
+/**
+ * Perform a pragma.
+ */
 #define __do_pragma(x)			_Pragma (#x)
+
+/**
+ * Perform a diagnostic pragma.
+ */
 #define __diagnostic_pragma(x)		__do_pragma(GCC diagnostic x)
+
+/**
+ * Perform a message pragma.
+ */
 #define __message_pragma(x)		__do_pragma(message __stringify(x))
 
 #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)
 
+/**
+ * Disable a diagnostic warning.
+ */
 #define __diagnostic_disable(x)				\
 	__diagnostic_pragma(push)			\
 	__diagnostic_pragma(ignored __concat(-W,x))
+
+/**
+ * Enable a diagnostic warning.
+ */
 #define __diagnostic_enable(x)		__diagnostic_pragma(pop)
+
+/**
+ * Send a build message.
+ */
 #define __build_message(x)		__message_pragma(x)
+
+/**
+ * Send a build warning.
+ */
 #define __build_warning(x)		__message_pragma(WARNING: x)
 
 #else /* 4.2 < GCC version > 4.2 */
@@ -126,17 +209,19 @@
 
 #endif /* __GNUC__ */
 
-/*
+#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+/**
  * Provide a means for warning when a function's result is unused.
  */
-#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
 #define __warn_unused_result __attribute__((warn_unused_result))
 #else
 #define __warn_unused_result
 #endif
 
-/* Provide a string identifying the current function */
 #ifdef __GNUC__
+/**
+ * Provides a string identifying the current function.
+ */
 #define STRFUNC		((const char*)(__PRETTY_FUNCTION__))
 #elif defined (__STDC_VERSION__) && __STDC_VERSION__ >= 19901L
 #define STRFUNC		((const char*)(__func__))
@@ -146,31 +231,60 @@
 #define STRFUNC		((const char*)("???"))
 #endif /* __GNUC__ */
 
-/* Provide a string identifying the current code position */
 #if defined (__GNUC__) && (__GNUC__ < 3)
-#define STRLOC							\
+#define STRLOC								\
 	__FILE__ ":" __stringify(__LINE__) ":" __PRETTY_FUNCTION__ "()"
 #else
+/**
+ * Provides a string identifying the current code position.
+ */
 #define STRLOC		__FILE__ ":" __stringify(__LINE__)
 #endif /* __GNUC__ */
 
-/* Provide integer boolean values */
 #ifndef FALSE
+/**
+ * Provide boolean false value
+ */
 #define FALSE (0)
 #endif
 
 #ifndef TRUE
+/**
+ * Provide boolean true value
+ */
 #define TRUE (!FALSE)
 #endif
 
-/* Provide convenient numerical operations */
+/**
+ * Return the maximum of two values.
+ */
 #define max(x,y)	(((x) > (y)) ? (x) : (y))
+
+/**
+ * Return the minimum of two values.
+ */
 #define min(x,y)	(((x) < (y)) ? (x) : (y))
+
+/**
+ * Return the absolute size of a value.
+ */
 #define abs(x)		(((x) < 0) ? -(x) : (x))
+
+/**
+ * Clamp a value in a bounded range.
+ */
 #define clamp(x, min, max) (((x) > (max)) ? (max) : (((x) < (min))	\
 						     ? (min) : (x)))
 
-/* Count the number of elements in a fixed-size array. */
+/**
+ * Count the number of elements in a fixed-size array.
+ */
 #define n_elements(a) (sizeof(a) / sizeof((a)[0]))
+
+/**
+ * Provides a way to round a non-negative integer up to the next multiple of
+ * sizeof(int). For example, sizeof(short) is 2, but _INTSIZEOF(short) is 4.
+ */
+#define _INTSIZEOF(n) ((sizeof(n) + sizeof(int) - 1) & ~(sizeof(int) - 1))
 
 #endif /* _MACROS_H */
